fec6f8dfa1fa249f3faa4eb5aeee18e5a1dd143e
trisjones <trisjones04@gmail.com>
2018-07-13 16:12:14 -0400

    Stuck on tweet_shortener

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def dictionary
+  {'hello' => 'hi',
+   'to' => '2',
+   'two' => '2',
+   'too' => '2',
+   'for' => '4',
+   'four' => '4',
+   'be' => 'b',
+   'you' => 'u',
+   'at' => '@',
+   'and' => '&',
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.each do |word|
+    dictionary.each do |og, slang|
+      if og == word
+        word = slang
+      else
+        word
+      end
+    end
+  end.join(' ')
+end
