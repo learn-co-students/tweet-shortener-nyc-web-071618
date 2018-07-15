d689082f03d084742c6292b9fc940d778ba06fd3
trisjones <trisjones04@gmail.com>
2018-07-15 18:35:11 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,13 +14,33 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  tweet.split.each do |word|
-    dictionary.each do |og, slang|
-      if og == word
-        word = slang
-      else
-        word
-      end
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
     end
   end.join(' ')
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.each do |message|
+    puts word_substituter(message)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + "..."
+  else
+    tweet
+  end
 end
